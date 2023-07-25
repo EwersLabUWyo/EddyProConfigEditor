3128391dde535a77f1a2af031c1030db5091dd01
Alex Fox <alextsfox@gmail.com>
2023-07-25 14:38:17 -0600

    parallel processing and ini file modification works now

--- eddypro/make_ini.py
+++ eddypro/make_ini.py
@@ -1,55 +1,195 @@
 import configparser
 from pathlib import Path
+import multiprocessing
+from math import ceil, floor
+from datetime import datetime
+from os import PathLike
 
 import numpy as np
 import pandas as pd
 
-eddypro_dir = Path('.')
-# read in ini file
-ini_fn = eddypro_dir/'ini/template.eddypro'
-ini = configparser.ConfigParser()
-ini.read(ini_fn)
 
-# break up processing into multiple separate jobs
-# from the template file, here are the start/end dates:
-# pr_end_date=2020-06-27
-# pr_end_time=23:30
-# pr_start_date=2020-06-26
-# pr_start_time=00:00
-job_size = '6H'
-starts = pd.date_range('2020-06-26 00:00', '2020-06-27 23:30', freq=job_size)
-ends = starts + pd.Timedelta(job_size) - pd.Timedelta('30m')  # dates are inclusive, so subtract 30min for file duration
-start_dates = starts.strftime(date_format=r'%Y-%m-%d')
-start_times = starts.strftime(date_format=r'%H:%M')
-end_dates = ends.strftime(date_format=r'%Y-%m-%d')
-end_times = ends.strftime(date_format=r'%H:%M')
+def modify_eddypro(
+        ref_fn: str | PathLike[str], 
+        out_fn: str | PathLike[str], 
+        FluxCorrection_SpectralAnalysis_General: dict = {},
+        Project: dict = {},
+        RawProcess_BiometMeasurements: dict = {},
+        RawProcess_General: dict = {},
+        RawProcess_ParameterSettings: dict = {},
+        RawProcess_Settings: dict = {},
+        RawProcess_Tests: dict = {},
+        RawProcess_TiltCorrection_Settings: dict = {},
+        RawProcess_TimelagOptimization_Settings: dict = {},
+        RawProcess_WindDirectionFilter: dict = {},
+) -> None:
+    """modifies a .eddypro config file
+    ini_fn: reference .eddypro file to modify
+    out_fn: path to write modified .eddypro files to
+    all other arguments: a dictionary-like object containing option-value pairs to modify within a given section. If an empty dict (default), then that section will not be modified.
+    """
 
-# give each project a unique id
-project_ids = [f'worker_{start}' for start in starts.strftime(date_format=r"%Y%m%d%H%M")]
-# where to save the file to
-ini_fns = [(eddypro_dir/f'ini/{project_id}.eddypro') for project_id in project_ids]
-# where to save outputs to
-out_path = eddypro_dir/'output'
+    ini = configparser.ConfigParser()
+    ini.read(ref_fn)
 
-# modify the ini files
-for i, fn in enumerate(ini_fns):
-    # empty ex_file I think?
-    ini.set(section='FluxCorrection_SpectralAnalysis_General', option='ex_file', value='')
-    ini.set(section='Project', option='file_name', value=fn.__str__())
-    ini.set(section='Project', option='last_change_date', value='')
-    ini.set(section='Project', option='pr_start_date', value=start_dates[i])
-    ini.set(section='Project', option='pr_end_date', value=end_dates[i])
-    ini.set(section='Project', option='pr_start_time', value=start_times[i])
-    ini.set(section='Project', option='pr_end_time', value=end_times[i])
-    ini.set(section='Project', option='project_id', value=project_ids[i])
-    ini.set(section='Project', option='proj_file', value=(eddypro_dir/'ini/lcreek.metadata').__str__())
-    # I guess set magnetic declination date to the processing date?
-    ini.set(section='RawProcess_General', option='dec_date', value=start_dates[i]) 
-    ini.set(section='Project', option='out_path', value=out_path.__str__())
-    ini.set(section='RawProcess_General', option='data_path', value=(eddypro_dir/'raw_data').__str__())
-    ini.set(section='Project', option='biom_dir', value=(eddypro_dir/'biomet').__str__())
+    for k, v in FluxCorrection_SpectralAnalysis_General.items():
+        ini.set(
+            section='FluxCorrection_SpectralAnalysis_General',
+            option=k,
+            value=str(v)
+        )
+    for k, v in Project.items():
+        ini.set(
+            section='Project',
+            option=k,
+            value=str(v)
+        )
+    for k, v in RawProcess_BiometMeasurements.items():
+        ini.set(
+            section='RawProcess_BiometMeasurements',
+            option=k,
+            value=str(v)
+        )
+    for k, v in RawProcess_General.items():
+        ini.set(
+            section='RawProcess_General',
+            option=k,
+            value=str(v)
+        )
+    for k, v in RawProcess_ParameterSettings.items():
+        ini.set(
+            section='RawProcess_ParameterSettings',
+            option=k,
+            value=str(v)
+        )
+    for k, v in RawProcess_Settings.items():
+        ini.set(
+            section='RawProcess_Settings',
+            option=k,
+            value=str(v)
+        )
+    for k, v in RawProcess_Tests.items():
+        ini.set(
+            section='RawProcess_Tests',
+            option=k,
+            value=str(v)
+        )
+    for k, v in RawProcess_TiltCorrection_Settings.items():
+        ini.set(
+            section='RawProcess_TiltCorrection_Settings',
+            option=k,
+            value=str(v)
+        )
+    for k, v in RawProcess_TimelagOptimization_Settings.items():
+        ini.set(
+            section='RawProcess_TimelagOptimization_Settings',
+            option=k,
+            value=str(v)
+        )
+    for k, v in RawProcess_WindDirectionFilter.items():
+        ini.set(
+            section='RawProcess_WindDirectionFilter',
+            option=k,
+            value=str(v)
+        )
 
-    # write the .ini file
-    with open(fn, 'w') as configfile:
+    with open(out_fn, 'w') as configfile:
         configfile.write(';EDDYPRO_PROCESSING\n')  # header line
         ini.write(fp=configfile, space_around_delimiters=False)
+
+    return
+
+def configure_parallel_workers(
+    ref_fn: str | PathLike[str], 
+    metadata_fn: str | PathLike[str] | None = None,
+    num_workers: int | None = None,
+    file_duration: int | None = None,
+) -> None:
+    """
+    given a .eddypro file, split it up into num_workers separate .eddypro files, each handling a separate time chunk.
+    all .eddypro files will be identical except in their project IDs, file names, and start/end dates.
+    
+    Note that some processing methods are not compatible "out-of-the-box" with paralle processing: some methods like the planar fit correction and ensemble spectral corrections will need the results from a previous, longer-term eddypro run to function effectively.
+
+    ref_fn: the base .eddypro file to modify when creating paralle workers.
+    metadata_fn: path to a static .metadata file for this project. Must be provided if file_duration is None.
+    num_workers: the number of parallel processes to configure. If None (default), then processing is split up according to the number of available processors on the machine.
+    file_duration: how many minutes long each file is. If None (Default), then that information will be gleaned from the metadata file.
+    """
+
+    # get file duration
+    if file_duration is None:
+        assert metadata_fn is not None, 'metadata_fn must be provided'
+        metadata = configparser.ConfigParser()
+        metadata.read(metadata_fn)
+        file_duration = int(metadata['Timing']['file_duration'])
+
+    if num_workers is None:
+        num_workers = max(multiprocessing.cpu_count() - 1, 1)
+
+    ini = configparser.ConfigParser()
+    ini.read(ref_fn)
+
+    # split up file processing dates
+    start = str(datetime.strptime(
+        f"{ini['Project']['pr_start_date']} {ini['Project']['pr_start_time']}", 
+        r'%Y-%m-%d %H:%M'
+    ))
+    end = str(datetime.strptime(
+        f"{ini['Project']['pr_end_date']} {ini['Project']['pr_end_time']}" , 
+        r'%Y-%m-%d %H:%M'
+    ))
+
+    n_files = len(pd.date_range(start, end, freq=f'{file_duration}min'))
+    job_size = ceil(file_duration*n_files/num_workers)
+    job_size = f'{int(ceil(job_size/file_duration)*file_duration)}min'
+
+    job_starts = pd.date_range('2020-06-21 00:00', '2020-07-22 00:00', freq=job_size)
+    job_ends = job_starts + pd.Timedelta(job_size) - pd.Timedelta(file_duration)  # dates are inclusive, so subtract 30min for file duration
+    job_start_dates = job_starts.strftime(date_format=r'%Y-%m-%d')
+    job_start_times = job_starts.strftime(date_format=r'%H:%M')
+    job_end_dates = job_ends.strftime(date_format=r'%Y-%m-%d')
+    job_end_times = job_ends.strftime(date_format=r'%H:%M')
+
+    # give each project a unique id and file name
+    project_ids = [f'worker{start}' for start in job_starts.strftime(date_format=r"%Y%m%d%H%M")]
+    ini_fns = [ref_fn.parent / f'{project_id}.eddypro' for project_id in project_ids]
+
+    # modify fns
+    for i, fn in enumerate(ini_fns):
+        ini['Project']['file_name'] = str(fn)
+        ini['Project']['pr_start_date'] = job_start_dates[i]
+        ini['Project']['pr_end_date'] = job_end_dates[i]
+        ini['Project']['pr_start_time'] = job_start_times[i]
+        ini['Project']['pr_end_time'] = job_end_times[i]
+        ini['Project']['project_id'] = project_ids[i]
+
+        with open(fn, 'w') as configfile:
+            configfile.write(';EDDYPRO_PROCESSING\n')  # header line
+            ini.write(fp=configfile, space_around_delimiters=False)
+
+    return
+
+
+
+if __name__ == '__main__':
+    project_dir = Path('/Users/alex/Documents/Work/UWyo/Research/Flux Pipeline Project/Eddypro-ec-testing/eddypro')
+    reference_ini_fn = project_dir/'ini/template.eddypro'
+    base_eddypro_fn = reference_ini_fn.parent / 'base.eddypro'
+    metadata_fn = project_dir / 'ini/lcreek.metadata'
+    out_path = project_dir/'output'
+    data_dir = Path('/Users/alex/Documents/Data/Platinum_EC/LostCreek')
+    
+    # modify template
+    modify_eddypro(
+        ref_fn=reference_ini_fn,
+        out_fn=base_eddypro_fn,
+        FluxCorrection_SpectralAnalysis_General=dict(ex_file=''),
+        Project=dict(last_change_date='', proj_file=metadata_fn, out_path=out_path, biom_dir=data_dir/'biomet'),
+        RawProcess_General=dict(data_path=data_dir/'raw_data')
+    )
+
+    configure_parallel_workers(
+        ref_fn=base_eddypro_fn,
+        metadata_fn=metadata_fn,
+    )
--- eddypro/run_eddypro.py
+++ eddypro/run_eddypro.py
@@ -1,33 +1,76 @@
-from tempfile import TemporaryFile
-from multiprocessing import Pool
+import multiprocessing
 import subprocess
 from pathlib import Path
 
-def call_rp_fcc(call):
-    print(call)
-    output = subprocess.check_output(call, shell=True)
-    return output
+def rm_tree(pth, root=True):
+            if root:
+                answer = input(f'\nDeleting {str(pth)}.\n"Y" to confirm... ')
+                assert answer.upper() == 'Y', f'Answered {answer}. Aborting!'
+            pth = Path(pth)
+            for child in pth.glob('*'):
+                if child.is_file():
+                    child.unlink()
+                else:
+                    rm_tree(child, root=False)
 
-def make_eddypro_calls():
+def call(args):
+    rp_call, fcc_call, stdout_file = args
+    with open(stdout_file, 'wb') as f:
+        try:
+            stdout = subprocess.check_output(rp_call, shell=True)
+            print(f'{stdout_file.stem} completed eddypro_rp gracefully')
+        except subprocess.CalledProcessError as e:
+            print(f'{stdout_file.stem} completed eddypro_rp with errors')
+            stdout = e.output
+        f.write(stdout)
+
+        try:
+            stdout = subprocess.check_output(fcc_call, shell=True)
+            print(f'{stdout_file.stem} completed eddypro_fcc gracefully')
+        except subprocess.CalledProcessError as e:
+            stdout = e.output
+            print(f'{stdout_file.stem} completed eddypro_fcc with errors')
+        f.write(stdout)
+
+def make_eddypro_calls(eddypro_dir):
     # location of eddypro rp and fcc
-    eddypro_rp = Path('./bin/eddypro_rp').__str__()
-    eddypro_fcc = Path('./bin/eddypro_fcc').__str__()
+    eddypro_rp = eddypro_dir/'bin/eddypro_rp'.__str__()
+    eddypro_fcc = eddypro_dir/'bin/eddypro_fcc'.__str__()
     # inputs to rp and fcc
     system="mac"
     mode="desktop"
-    environment = Path('./').__str__()
-    ini_dir = Path("./ini")
+    environment = eddypro_dir.__str__()
+    ini_dir = eddypro_dir/'ini'
     project_files = [fn.__str__() for fn in ini_dir.glob("worker*.eddypro")]
     # construct call
-    eddypro_rp_calls = [f'"{eddypro_rp}" -s "{system}" -m "{mode}" -e "{environment}" "{PROJ_FILE}"' for PROJ_FILE in project_files]
-    eddypro_fcc_calls = [f'"{eddypro_fcc}" -s "{system}" -m "{mode}" -e "{environment}" "{PROJ_FILE}"' for PROJ_FILE in project_files]
-    full_call_lst = [f'{rp_call} && {fcc_call}' for rp_call, fcc_call in zip(eddypro_rp_calls, eddypro_fcc_calls)]
+    eddypro_rp_calls = [
+        f'"{eddypro_rp}" -s "{system}" -m "{mode}" -e "{environment}" "{PROJ_FILE}"'
+        for PROJ_FILE in project_files
+    ]
+    eddypro_fcc_calls = [
+        f'"{eddypro_fcc}" -s "{system}" -m "{mode}" -e "{environment}" "{PROJ_FILE}"'
+        for PROJ_FILE in project_files
+    ]
 
-    return full_call_lst
+    return eddypro_rp_calls, eddypro_fcc_calls, project_files
 if __name__ == '__main__':
-    full_call_lst = make_eddypro_calls()
-    with Pool(4) as p: 
-        outputs = p.map(call_rp_fcc, full_call_lst)
-        for i, out in enumerate(outputs):
-            with open(Path(f'./{i}.log'), 'w') as logfile:
-                logfile.write(out)
+    eddypro_dir = Path('/Users/alex/Documents/Work/UWyo/Research/Flux Pipeline Project/Eddypro-ec-testing/eddypro')
+    stdout_dir = Path('/Users/alex/Documents/Work/UWyo/Research/Flux Pipeline Project/Eddypro-ec-testing/eddypro/stdout')
+    eddypro_rp_calls, eddypro_fcc_calls, project_files = make_eddypro_calls(eddypro_dir)
+    
+    clear_output = True
+    if clear_output:
+        rm_tree(eddypro_dir / 'output')
+        Path.mkdir(eddypro_dir / 'output', exist_ok=True)
+        rm_tree(eddypro_dir / 'stdout')
+        Path.mkdir(eddypro_dir / 'stdout', exist_ok=True)
+        rm_tree(eddypro_dir / 'tmp')
+        Path.mkdir(eddypro_dir / 'tmp', exist_ok=True)
+
+    stdout_files = [stdout_dir / (Path(f).stem + '.stdout') for f in project_files]
+    args = [(rp_call, fcc_call, f) for rp_call, fcc_call, f in zip(eddypro_rp_calls, eddypro_fcc_calls, stdout_files)]
+
+    processes = max(multiprocessing.cpu_count() - 2, 1)
+    print(f'\nBeginning EddyPro Runs: {len(args)} runs on {processes} cores\n')
+    with multiprocessing.Pool(processes) as p:
+        p.map(call, args)
